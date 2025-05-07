59683dba612f6789601e45e4f31d2cc208398cc3
Fabiano <radbios@gmail.com>
2025-05-07 16:54:29 -0300

    feat: include new infos into auth token

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,7 +33,10 @@ class User extends Authenticatable implements JWTSubject
      */
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
     }
 
     
