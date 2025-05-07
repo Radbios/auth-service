5c114808648c5ff750dfca92b5da09d34aa62e6e
Fabiano <radbios@gmail.com>
2025-05-07 16:28:54 -0300

    feat: logout

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,12 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 Route::middleware('api_guest')->group(function () {
     Route::post('login', [AuthController::class, "authenticate"])->name('login')->middleware('throttle:3,1');
 });
 
 Route::middleware("auth:api")->group(function() {
-
+    Route::post('logout', [AuthController::class, "logout"])->name('logout');
 });
